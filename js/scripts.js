ca86b7950847a76bd30d4eddade5f3239d351ad2
nic <netzeln@gmail.com>
2016-01-22 08:03:16 -0800

    pass third spec test for divide by15

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,10 @@
 var pingPong = function(userInput){
-  if(userInput % 3 == 0 && !(userInput % 5 === 0)){
+  if(userInput % 3 === 0 && !(userInput % 5 === 0)){
     return "Ping";
-  } else if(userInput % 5 == 0 && !(userInput % 3 === 0)){
+  } else if(userInput % 5 === 0 && !(userInput % 3 === 0)){
     return "Pong";
+  } else if (userInput % 15 === 0){
+    return "Ping Pong";
   }
 }
 $(document).ready(function() {
--- spec/specs.js
+++ spec/specs.js
@@ -6,4 +6,8 @@ describe("pingPong", function(){
   it("returns 'Pong' if a number is divisible by 5 but not 3", function(){
     expect(pingPong(10)).to.equal("Pong");
   });
+
+  it("returns 'Ping Pong' if a number is divisible by both 5 and 3", function(){
+    expect(pingPong(15)).to.equal("Ping Pong");
+  });
 });
