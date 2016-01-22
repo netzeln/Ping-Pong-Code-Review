97c9bca45f7a42a80a9d5435d650e194ea8fd6c1
nic <netzeln@gmail.com>
2016-01-22 10:10:48 -0800

    make second version function work as expected

--- js/scripts2.js
+++ js/scripts2.js
@@ -1,20 +1,37 @@
-
-var countUp = function(userInput) {
-
-  for(var i = 1; i <= userInput; i++){
-    if(i % 3 === 0 && !(i % 5 === 0)){
-        $("ul").append("<li>Ping</li>");
-    } else if(i % 5 === 0 && !(i % 3 === 0)){
-        $("ul").append("<li>Pong</li>");
-    } else if (i % 15 === 0){
-        $("ul").append("<li>Ping Pong</li>");
-    } else{
-        $("ul").append("<li>" + i + "</li>");
-  }
+//
+// var countUp = function(userInput) {
+//
+//   for(var i = 1; i <= userInput; i++){
+//     if(i % 3 === 0 && !(i % 5 === 0)){
+//         $("ul#results").append("<li>Ping</li>");
+//     } else if(i % 5 === 0 && !(i % 3 === 0)){
+//         $("ul#results").append("<li>Pong</li>");
+//     } else if (i % 15 === 0){
+//         $("ul#results").append("<li>Ping Pong</li>");
+//     } else{
+//         $("ul#results").append("<li>" + i + "</li>");
+//   }
 
 
 $(document).ready(function() {
 
+$("form#getNumber").submit(function(event){
 
+  var userInput = Math.abs(parseInt($("input#countUp").val()))
+  $("ul#results").empty();
+  for(var i = 1; i <= userInput; i++){
+    if(i % 3 === 0 && !(i % 5 === 0)){
+        $("ul#results").append("<li>Ping</li>");
+    } else if(i % 5 === 0 && !(i % 3 === 0)){
+        $("ul#results").append("<li>Pong</li>");
+    } else if (i % 15 === 0){
+        $("ul#results").append("<li>Ping Pong</li>");
+    } else{
+        $("ul#results").append("<li>" + i + "</li>");
+    }
+    }
+    $(".resultslist").show();
 
+    event.preventDefault();
+  });
 });
